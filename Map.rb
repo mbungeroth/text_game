b996dfd90971741939e993211a7b71e72687d131
mbungeroth <31143755+mbungeroth@users.noreply.github.com>
2017-08-24 10:10:42 -0400

    Create Map.rb
    how to get from one place to the other

---
+++ Map.rb
@@ -0,0 +1,18 @@
+class Map
+  @@rooms = {bedroom: Bedroom.new(),
+             living_room: LivingRoom.new(),
+             finished: Finished.new()}
+
+  def initialize(start_room)
+    @start_room = start_room
+  end
+
+  def next_room(room_name)
+    the_room = @@rooms[room_name]
+    the_room
+  end
+
+  def opening_room()
+    next_room(@start_room)
+  end
+end
